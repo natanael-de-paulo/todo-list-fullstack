ac74ba490617708aa3e0ad9514c3be55b086d301
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-07-07 08:41:36 -0300

    refact: changed constructor parameter name

--- backend/src/app/task/controllers/update-task-controller.ts
+++ backend/src/app/task/controllers/update-task-controller.ts
@@ -3,12 +3,12 @@ import { DataTaskToUpdateDTO } from '../dtos/data-task-to-update-dto'
 import { ITasksServices } from '../services/interfaces/task-service'
 
 class UpdateTaskController{
-  constructor(private getTaskService: ITasksServices<DataTaskToUpdateDTO, void>){}
+  constructor(private updateTaskService: ITasksServices<DataTaskToUpdateDTO, void>){}
   
   async handle(req: Request, res: Response){
     const { userId, taskId } = req.params
     const { name, status } = req.body
-    const respose = await this.getTaskService.execute({userId, taskId, name, status})
+    const respose = await this.updateTaskService.execute({userId, taskId, name, status})
     return res.json(respose)
   }
 }
