73761fa1bc32ff4c2f0af16898e043190ed49321
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-07-14 11:58:34 -0300

    feat: create docker image backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,7 @@
 import app from './app'
 
-const hostname = process.env.SERVER_HOST || 'localhost'
-const port = Number(process.env.SERVER_PORT) || 3001
+const port = Number(process.env.SERVER_PORT) 
 
-app.listen(port, hostname, () => {
-  console.log(`server is listening on ${hostname}:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`)
 })
