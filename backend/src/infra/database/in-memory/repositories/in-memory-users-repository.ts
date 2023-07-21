0b0136b9350b718613f258a4704650b7d85b1611
Natanael de Paulo <natanaeldepaulo123@gmail.com>
2023-07-20 22:47:50 -0300

    feat: in memory database

---
+++ backend/src/infra/database/in-memory/repositories/in-memory-users-repository.ts
@@ -0,0 +1,82 @@
+import { createUserResponseDTO } from "../../../../app/user/dtos/create-user-dto-response";
+import { IUsersRepository } from "../../../../app/user/repository/users-repository";
+import prisma from "../../../../libs/prisma"
+import { createUserRequestDTO } from "../../../../app/user/dtos/create-user-dto-request";
+import { dataUserToUpdateDTO } from "../../../../app/user/dtos/data-user-dto-to-update";
+import { UserResponseDTO } from "../../../../app/user/dtos/user-dto-response";
+import { UserDataOutputDTO } from "../../../../app/auth/dtos/user-data-output-dto";
+
+class InMemoryUsersRepository implements IUsersRepository {
+
+  async getUserForAuth(email: string): Promise<UserDataOutputDTO | null> {
+    const query = await prisma.user.findUnique({
+      where: {
+        email: email
+      },
+      select: {
+        userId: true,
+        password: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
+
+    return query 
+  }
+
+
+  async get(userId: string): Promise<UserResponseDTO> {
+    const query = await prisma.user.findUnique({
+      where: {
+        userId: userId,
+      },
+      select: {
+        userId: true,
+        name: true,
+        email: true,
+        tasks: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
+
+    return query as UserResponseDTO 
+  }
+
+  async create(input: createUserRequestDTO): Promise<createUserResponseDTO> {
+    const output = await prisma.user.create({
+      data: {
+        ...input
+      } as createUserRequestDTO,
+      select: {
+        userId: true,
+      }
+    })
+
+    return output
+  }
+
+  async delete(userId: string): Promise<void> {
+    await prisma.user.delete({
+      where: {
+        userId: userId
+      }
+    })
+  }
+
+  async update(input: dataUserToUpdateDTO): Promise<void> {
+    await prisma.user.update({
+      data: {
+        ...input.dataUserToUpdate
+      },
+      where: {
+        userId: input.userId
+      }
+    })
+  }
+}
+
+
+export default InMemoryUsersRepository
